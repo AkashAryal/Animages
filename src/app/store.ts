cc385c7e8bb55d421b999d172bfe1ef66c14b0da
Akash Aryal <aryal.akash4414@gmail.com>
2020-06-18 13:51:53 -0500

    init

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,12 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import { configureStore, ThunkAction, Action, getDefaultMiddleware } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import logger from 'redux-logger';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
   },
+  middleware: [...getDefaultMiddleware(), logger]
 });
 
 export type RootState = ReturnType<typeof store.getState>;
