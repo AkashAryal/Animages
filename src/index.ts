1946e65474c176b3f6b5b345f42a2357151c26e9
Akash Aryal <aryal.akash4414@gmail.com>
2020-07-17 18:04:57 -0500

    updated path for index.ts of server

--- src/index.ts
+++ src/index.ts
@@ -30,9 +30,9 @@ app.use(function (req: Request, res: Response) {
 });
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static("../../build"));
+  app.use(express.static("client/build"));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, "../../build/index.html"));
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   })
 }
 
